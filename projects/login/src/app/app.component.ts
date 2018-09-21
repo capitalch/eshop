281b7e38a715e2a6df3c5e46f7e148f129cb2ca3
SushantAgrawal <sagarwal@netwoven.com>
2018-09-21 12:44:12 +0530

    init

--- projects/login/src/app/app.component.ts
+++ projects/login/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IbukiService } from 'ibuki';
-// import { bcrypt } from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,14 +17,40 @@ export class AppComponent {
 
   // base64(username:Hash of passowrd)
 
-  login() {
-    const saltRounds = 10;
-    const myPlaintextPassword = 's0/\/\P4$$w0rD';
-    const someOtherPlaintextPassword = 'not_bacon';
+  encrypt() {
+    const base64Key = crypto.enc.Hex.parse('0123456789abcdef0123456789abcdef');
+    const iv = crypto.enc.Hex.parse('abcdef9876543210abcdef9876543210');
 
-    bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-      const x = 0;
-    });
+    const encrypted = crypto.AES.encrypt(
+      'test',
+      base64Key,
+      { iv: iv }
+    );
+    console.log(encrypted.ciphertext.toString(crypto.enc.Base64));
+  }
+  // var aesUtil = new AesUtil(keySize, iterationCount);
+  // var plaintext =  aesUtil.decrypt(salt, iv, encryptionKey, dataToDecrypt);
+  // console.log(plaintext);
+
+  decrypt() {
+    // const aesutil = new AesUtil(128, 10);
+  }
+
+  endecr() {
+    const encrypted = crypto.AES.encrypt('Message', 'Secret Passphrase');
+    const text = encrypted.toString(); console.log(text);
+    const decrypted = crypto.AES.decrypt(text, 'Secret Passphrase');
+    console.log(decrypted.toString(crypto.enc.Utf8));
+  }
+
+  login() {
+    this.endecr();
+
+
+
+    // bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+    //   const x = 0;
+    // });
   }
 
 }
--- projects/login/src/app/app.module.ts
+++ projects/login/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from 'angular-material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {IbukiModule} from 'ibuki';
+import { IbukiModule } from 'ibuki';
 @NgModule({
   declarations: [
     AppComponent
